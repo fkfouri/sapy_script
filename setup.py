9b744e085804bf4ded0c5928b0f6d45e5f7c2487
fabio kfouri <kfouri.fabio@gmail.com>
2018-07-25 12:56:37 -0300

    Adequacao do setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
+		"Programming Language :: Other Scripting Engines",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows 7",
-        "Operating System :: Windows 8",
-        "Intended Audience :: Developers",
-        "Intended Audience :: SAP :: Sap Script",
-        ""
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 8",
+        "Intended Audience :: Developers"
     ),
     keywords="SAP Script",
     install_requires=['pywin32==223', 'tqdm==4.20.0', 'wmi==1.4.9']
