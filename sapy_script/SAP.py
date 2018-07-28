2237833c5cef5f20ef4491af6ce2588c8d4d3348
Pyp Boy <41709805+pypboy@users.noreply.github.com>
2018-07-28 20:05:00 -0300

    Implementação de objeto Session
    Foi implementado o objeto Session para segregação de atividades, assim removendo métodos estáticos sendo utilizado passando a "session" com parâmetro.
    Remoção de algumas redundâncias nas funções.

--- sapy_script/SAP.py
+++ sapy_script/SAP.py
@@ -5,9 +5,8 @@ from win32com.client import GetObject
 from subprocess import Popen
 from collections import Iterable
 from tqdm import tqdm
-from uuid import uuid4
-from os import getpid, pathsep, remove, path
-from tempfile import gettempdir
+from os import getpid
+from sapy_script.Session import Session
 
 session_process = None
 all_processes_id = []
@@ -21,7 +20,8 @@ def _on_init(sid, p_ids):
     while True:
         con = app.Children(i)
         if con.Children(0).Info.systemsessionid == sid:
-            session_process = con.Children(p_ids.index(getpid()))
+            session = con.Children(p_ids.index(getpid()))
+            session_process = Session(session)
             break
         i = i + 1
 
@@ -36,8 +36,7 @@ class SAP:
         self._con = None
         self._tasks = []
         self.max_sessions = max_sessions
-        self.session = lambda i=0: self._con.Children(i)
-        self._connected = False
+        self.session = lambda i=0: Session(self._con.Children(i))
 
     @staticmethod
     def app():
@@ -56,28 +55,9 @@ class SAP:
                 sleep(1)
                 pass
 
-
-    def get_sbar_status(self, session):
-        return session.findById("wnd[0]/sbar/pane[0]").text
-
-    def window_caption(self, session):
-        return session.ActiveWindow.Text
-
-    def test_connection(self, session):
-        """SAP connection test"""
-        try:
-            session.findById("wnd[0]/tbar[0]/okcd").text = "/n"
-            session.findById("wnd[0]").sendVKey(0)
-            session.findById("wnd[0]/usr/btnSTARTBUTTON").press()
-            return True
-        except:
-            return False
-            raise ValueError('SAP was not connected.')
-
-
     def connect(self, environment, client=None, user=None, password=None, lang=None, force=False):
         con = SAP.app().OpenConnection(environment, True)
-        session = con.Children(0)
+        session = Session(con.Children(0))
 
         if client is not None:
             session.findById("wnd[0]/usr/txtRSYST-MANDT").Text = client
@@ -121,16 +101,18 @@ class SAP:
                 pass
 
         # Teste da Conexao
-        if self.test_connection(session):
-            self._connected = True
+        connected = session.is_connected()
 
-        del session
-        self._con = con
-        return self._connected
+        if session.is_connected():
+            self._con = con
+            return True
+        
+        self._con = None
+        return False
 
     @property
     def connected(self):
-        return self._connected
+        return self.session().is_connected()
 
     @staticmethod
     def session():
@@ -207,25 +189,6 @@ class SAP:
         self.clear_tasks()
         return response
 
-    @staticmethod
-    def append_multi_selection(data):
-        if type(data) is not str:
-            data = "\r".join([str(d) for d in data])
-
-        folder = gettempdir() + pathsep
-        file_name = "temp_append_{}_{}.txt".format(uuid4().hex, str(getpid()))
-        file_address = path.join(folder[:-1], file_name)
-        with open(file_address, 'w+') as f:
-            f.write(data)
-        del f
-        session = SAP.session()
-        session.findById("wnd[1]/tbar[0]/btn[16]").press()
-        session.findById("wnd[1]/tbar[0]/btn[23]").press()
-        session.findById("wnd[2]/usr/ctxtDY_PATH").text = folder[:-1]
-        session.findById("wnd[2]/usr/ctxtDY_FILENAME").text = file_name
-        session.findById("wnd[2]").sendVKey(0)
-        session.findById("wnd[1]").sendVKey(8)
-        remove(file_address)
 
     @staticmethod
     def multi_arguments(func):
